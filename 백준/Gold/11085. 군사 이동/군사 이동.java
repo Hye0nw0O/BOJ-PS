cf6930606061399b4824030e8dc17419644a253f
Hye0nw0O <145957083+Hye0nw0O@users.noreply.github.com>
2024-08-04 15:24:32 +0900

    [Gold III] Title: 군사 이동, Time: 300 ms, Memory: 32124 KB -BaekjoonHub

---
+++ 백준/Gold/11085. 군사 이동/군사 이동.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        arr = new int[n];
+        for (int i=0; i<n; i++) { arr[i] = i; }
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(bf.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+        for (int i=0; i<m; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            pq.add(new Edge(u, v, w));
+        }
+        int res = 1001;
+        while (!pq.isEmpty()) {
+            Edge now = pq.poll();
+            if (find(now.s) != find(now.e)) {
+                union(now.s, now.e);
+                res = Math.min(now.w, res);
+            }
+            if (find(start) == find(end)) break;
+        }
+        System.out.println(res);
+    }
+    public static void union(int a, int b) {
+        a = find(a); b = find(b);
+        if (a != b) { arr[b] = a; }
+    }
+    public static int find(int n) {
+        if (arr[n] == n) {
+            return n;
+        } else {
+            return arr[n] = find(arr[n]);
+        }
+    }
+}
+class Edge implements Comparable<Edge> {
+    int s, e, w;
+    public Edge(int s, int e, int w) {
+        this.s = s; this.e = e; this.w = w;
+    }
+    public int compareTo(Edge E) {
+        return E.w - this.w;
+    }
+}
