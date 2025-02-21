515ee69839f1825d8575fcd2125a111184be7efe
Hye0nw0O <145957083+Hye0nw0O@users.noreply.github.com>
2025-02-21 17:38:41 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1148 ms, Memory: 59460 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,36 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
-public class Main {
-	static int[] array;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		
-		st = new StringTokenizer(br.readLine());
-		array = new int [n+1];
-		
-		for (int i=1; i<=n; i++) {
-			array[i] = array[i-1]+Integer.parseInt(st.nextToken());
-		}
-		
-		
-		for (int i=0; i<m; i++) {
-			st=new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			System.out.println(array[b] - array[a-1]);
-		}
-		
-		
-	}
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        int[] sum = new int[n + 1];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (i == 0) {
+                sum[i] = arr[i];
+            } else {
+                sum[i] += sum[i - 1] + arr[i];
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (x == 1) {
+                System.out.println(sum[y]);
+            } else if (x == y) {
+                System.out.println(arr[y]);
+            } else {
+                System.out.println(sum[y] - sum[x-1]);
+            }
+        }
+    }
 }
