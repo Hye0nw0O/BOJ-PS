983217da7b81e6ec2154e173d79984c3e3e65d2d
Hye0nw0O <145957083+Hye0nw0O@users.noreply.github.com>
2025-03-04 18:46:32 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 428 ms, Memory: 17496 KB -BaekjoonHub

---
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int k;
+    public static int cnt;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int size = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(bf.readLine());
+        int[] arr = new int [size];
+        for (int i=0; i<size; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        bubble_sort(arr);
+        if (cnt < k) {
+            System.out.println(-1);
+        }
+    }
+    public static void bubble_sort (int[] arr) {
+        cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                int tmp1 = arr[j];
+                int tmp2 = arr[j+1];
+                if (tmp1 > tmp2) {
+                    cnt++;
+                    if (cnt == k) {
+                        System.out.print(tmp2 + " " + tmp1);
+                        return;
+                    }
+                    arr[j+1] = tmp1;
+                    arr[j] = tmp2;
+                }
+            }
+        }
+    }
+}
