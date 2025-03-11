a29d28ce40715a6fda23d533bbe9a895ccb8a899
Hye0nw0O <145957083+Hye0nw0O@users.noreply.github.com>
2025-03-11 18:25:24 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 1, Time: 220 ms, Memory: 16384 KB -BaekjoonHub

---
+++ 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int k;
+    public static int cnt;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int size = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(bf.readLine());
+        int[] arr = new int[size + 1];
+        for (int i = 1; i <= size; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        insertion_sort(arr);
+        if (cnt < k) {
+            System.out.println(-1);
+        }
+    }
+
+    public static void insertion_sort(int[] arr) {
+        for (int i = 2; i < arr.length; i++) {
+            int loc = i - 1;
+            int newItem = arr[i];
+            while (1 <= loc && newItem < arr[loc]) {
+                arr[loc + 1] = arr[loc];
+                cnt++;
+                if (cnt == k) {
+                    System.out.println(arr[loc]);
+                    return;
+                }
+                loc--;
+            }
+            if (loc + 1 != i) {
+                arr[loc + 1] = newItem;
+                cnt++;
+                if (cnt == k) {
+                    System.out.println(newItem);
+                    return;
+                }
+            }
+        }
+    }
+}
