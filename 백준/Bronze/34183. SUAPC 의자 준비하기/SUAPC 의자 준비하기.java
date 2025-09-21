c9f7de739f53f34f5f7031f66f763976f0bda3fb
Hye0nw0O <145957083+Hye0nw0O@users.noreply.github.com>
2025-09-21 18:00:50 +0900

    [Bronze IV] Title: SUAPC 의자 준비하기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/34183. SUAPC 의자 준비하기/SUAPC 의자 준비하기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int res = (3 * n - m) * a + b;
+        if (3 * n <= m) System.out.println(0);
+        else System.out.println(res);
+    }
+}
